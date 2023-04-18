d691c6e105995115c8a291a407bc6d40a4121bd6
hi_shadow <vazagautam54@gmail.com>
2023-04-18 19:03:42 +0530

    added : Order Endpoint

--- SERVER/Controllers/orderController.js
+++ SERVER/Controllers/orderController.js
@@ -1,4 +1,5 @@
 const Orders = require("../Models/Order_Model")
+const Products = require("../Models/Products_Model")
 const Users = require("../Models/User_Model")
 const ApiFeatures = require("../Utils/ApiFeatures")
 const ErrorHandler = require("../Utils/ErrorHandler")
@@ -15,16 +16,14 @@ exports.NewOrder = AsyncErrors(async (req, res, next) => {
         res.status(200).json({
                 success: true,
                 order
-
         })
-
 })
 
 
 // Get Single Order 
 
 exports.GetSingleOrder = AsyncErrors(async (req, res, next) => {
-        const order = await Orders.findById(req.params.id).populate("user" , "name , email ")
+        const order = await Orders.findById(req.params.id).populate("user", "name , email ")
         if (!order) { return next(new ErrorHandler(404, "Order Not Found")) }
         res.status(200).json({
                 success: true,
@@ -32,7 +31,7 @@ exports.GetSingleOrder = AsyncErrors(async (req, res, next) => {
         })
 })
 
-// All Orders Login users
+// All Orders -- LoggedIn User 
 exports.MyOrders = AsyncErrors(async (req, res, next) => {
         const order = await Orders.find({ user: req.user._id })
         const TotalOrders = await Orders.find({ user: req.user._id }).countDocuments()
@@ -44,3 +43,67 @@ exports.MyOrders = AsyncErrors(async (req, res, next) => {
         })
 })
 
+// Get All Orders --Admin
+exports.GetAllOrders = AsyncErrors(async (req, res, next) => {
+
+        const orders = await Orders.find()
+        let TotalAmount = 0
+        orders.forEach((order) => {
+                TotalAmount += order.totalPrice
+        })
+        res.status(200).json({
+                status: 200,
+                TotalAmount,
+                orders
+        })
+})
+
+// Update Orders Status
+exports.GetAllOrders = AsyncErrors(async (req, res, next) => {
+
+        const orders = await Orders.findById(req.params.id)
+        if (orders.orderStatus === "Delivered") {
+                return next(new ErrorHandler(404, "You Have Already Delivered This Order"))
+        }
+        res.status(200).json({
+                status: 200,
+                TotalAmount,
+                orders
+        })
+})
+
+
+// Update Order Status
+
+exports.UpdateOrder = AsyncErrors(async (req, res, next) => {
+
+        const order = await Orders.findById(req.params.id)
+        if (!order) { return next(new ErrorHandler(404, "Order Not Found")) }
+
+        order.orderStatus = req.body.orderStatus
+        if (req.body.orderStatus === "Delivered") {
+                order.deliveredAt = Date.now()
+        }
+        order.orderItems.forEach(async (order) => {
+                await UpdateStock(order.product, order.quantity)
+        });
+        await order.save({ validateBeforeSave: false })
+        res.status(200).json({
+                success: true,
+        })
+})
+
+async function UpdateStock(ProductId, Quantity) {
+        const product = await Products.findById(ProductId)
+        if (!product) { return next(new ErrorHandler(404, "Product Not Found")) }
+        product.stock -= Quantity
+        product.save({ validateBeforeSave: false })
+}
+
+exports.DeleteOrder = AsyncErrors(async (req, res, next) => {
+        const order = await Orders.findByIdAndDelete(req.params.id)
+        if (!order) { return next(new ErrorHandler(404, "Order Not Found")) }
+        res.status(200).json({
+                success: true,
+        })
+})
--- SERVER/Routes/OrderRoute.js
+++ SERVER/Routes/OrderRoute.js
@@ -1,12 +1,18 @@
 const express = require("express")
 const router = express.Router()
 const { AuthenticatedUser, AuthoriseRole } = require("../middleware/Authentication")
-const { NewOrder, GetSingleOrder, MyOrders } = require("../Controllers/orderController")
+const { NewOrder, GetSingleOrder, MyOrders, UpdateOrder, GetAllOrders, DeleteOrder } = require("../Controllers/orderController")
 
 
 router.route("/").get(AuthenticatedUser, MyOrders)
 router.route("/new").post(AuthenticatedUser, NewOrder)
 router.route("/:id").get(AuthenticatedUser, AuthoriseRole("admin"), GetSingleOrder)
+router.route("/admin/orders").get(AuthenticatedUser, AuthoriseRole("admin"), GetAllOrders)
+
+
+
+router.route("/admin/orders/:id").put(AuthenticatedUser, AuthoriseRole("admin"), UpdateOrder)
+router.route("/:id").delete(AuthenticatedUser, DeleteOrder)
 
 
 module.exports = router
